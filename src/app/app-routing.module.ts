d8c9ca03828fb37d3576e6597cadf5916d25348a
omarl68 <omar.lab68@gmail.com>
2022-07-31 20:19:39 +0100

    secand commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,39 @@
-import { NgModule } from '@angular/core';
+import { dashCaseToCamelCase } from '@angular/compiler/src/util';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './compoment/privet/dashboard/dashboard.component';
+import { AddProductComponent } from './compoment/privet/products/add-product/add-product.component';
+import { ProductListComponent } from './compoment/privet/products/product-list/product-list.component';
+import { UpdateProductComponent } from './compoment/privet/products/update-product/update-product.component';
+import { HomeComponent } from './compoment/public/home/home.component';
+import { PanierComponent } from './compoment/public/panier/panier.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: HomeComponent
+  },
+  {
+    path:'panier',
+    component: PanierComponent
+  },
+  {
+    path:'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path:'add-product',
+    component: AddProductComponent
+  },
+  {
+    path:'porduct-list',
+    component: ProductListComponent
+  },
+  {
+    path:'update-product',
+    component: UpdateProductComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './compoment/public/home/home.component';
+import { PanierComponent } from './compoment/public/panier/panier.component';
+import { DashboardComponent } from './compoment/privet/dashboard/dashboard.component';
+import { NavbarComponent } from './compoment/public/navbar/navbar.component';
+import { DisplayProductsComponent } from './compoment/public/display-products/display-products.component';
+import { SideBarComponent } from './compoment/privet/side-bar/side-bar.component';
+import { TopBarComponent } from './compoment/privet/top-bar/top-bar.component';
+import { ProductListComponent } from './compoment/privet/products/product-list/product-list.component';
+import { AddProductComponent } from './compoment/privet/products/add-product/add-product.component';
+import { UpdateProductComponent } from './compoment/privet/products/update-product/update-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PanierComponent,
+    DashboardComponent,
+    NavbarComponent,
+    DisplayProductsComponent,
+    SideBarComponent,
+    TopBarComponent,
+    ProductListComponent,
+    AddProductComponent,
+    UpdateProductComponent
   ],
   imports: [
     BrowserModule,
